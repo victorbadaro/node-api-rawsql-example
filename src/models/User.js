3aeda7cd7fc2dd2e92cbede866dda8257f4dac53
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-23 15:24:03 -0300

    feat: add model User

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+import { connection as database } from '../config/database';
+
+class User {
+    async find() {
+
+    }
+
+    async create() {
+
+    }
+
+    async update() {
+
+    }
+
+    async delete() {
+
+    }
+}
+
+export default new User();
