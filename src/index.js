0d4e93bd3e9a22bc5ef3d0bd43bbe34016c3b848
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-23 12:36:39 -0300

    feat: add routes file and set the server to receive requests with json content

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 const SERVER_PORT = process.env.SERVER_PORT;
+
+app.use(express.json());
+app.use(router);
 
 app.listen(SERVER_PORT, () => console.log(`Server is running! Port: ${SERVER_PORT}`));
